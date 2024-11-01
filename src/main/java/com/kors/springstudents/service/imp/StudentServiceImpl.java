77d888b17f9fa6a7ebcd7c0745c7f0b6b6613bb1
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2024-11-01 11:10:28 +0500

    Add connection to db, service and repository

--- src/main/java/com/kors/springstudents/service/imp/StudentServiceImpl.java
+++ src/main/java/com/kors/springstudents/service/imp/StudentServiceImpl.java
@@ -1,39 +1,45 @@
 package com.kors.springstudents.service.imp;
 
 import com.kors.springstudents.model.Student;
-import com.kors.springstudents.repository.InMemoryStudentDAO;
+import com.kors.springstudents.repository.StudentRepository;
+import com.kors.springstudents.service.StudentService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class StudentService implements com.kors.springstudents.service.StudentService {
-    private final InMemoryStudentDAO repository;
+@Primary
+public class StudentServiceImpl implements StudentService {
+    private final StudentRepository repository;
+
 
     @Override
     public List<Student> findAllStudent() {
-        return repository.findAllStudent();
+        return repository.findAll();
     }
 
     @Override
     public Student saveStudent(Student student) {
-        return repository.saveStudent(student);
+        return repository.save(student);
     }
 
     @Override
     public Student findByEmail(String email) {
-        return repository.findByEmail(email);
+        return repository.findStudentByEmail(email);
     }
 
     @Override
     public Student updateStudent(Student student) {
-        return repository.updateStudent(student);
+        return repository.save(student);
     }
 
     @Override
+    @Transactional
     public void deleteStudent(String email) {
-        repository.deleteStudent(email);
+        repository.deleteByEmail(email);
     }
 }
